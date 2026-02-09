8429cb644d96c4d5688919da23b681fe69ea3e65
Lunacywonders <Isaacn9394@gmail.com>
2026-02-08 23:06:01 -0500

    asd

--- main.js
+++ main.js
@@ -5,14 +5,40 @@ const paper = document.querySelector(".paper");
 const scissors = document.querySelector(".scissors");
 
 function randomize(deck) {
-  let random = Math.floor(Math.random() * 4);
+  let random = Math.floor(Math.random() * 3);
   let bigdawg = deck[random];
   return bigdawg;
 }
 
-rock.addEventListener("click", function() {
-console.log(randomize(deck));
-});
-
-function game(rps){
+function computer(rps){
+  let comprandom = randomize(rps)
+  return comprandom
 }
+
+scissors.addEventListener("click", function(){
+game("Scissors");
+})
+
+rock.addEventListener("click", function(){
+game("Rock");
+})
+
+paper.addEventListener("click", function(){
+game("Paper");
+})
+
+function game(user){
+  let computerChoice = computer(rps)
+  rpsi.innerHTML = `You chose: ${user} <br><br> Computer chose: ${computerChoice}`;
+  if (user === computerChoice){
+    rpsi.innerHTML += "<br><br>It's a tie!"
+  } else if ((user === "Rock" && computerChoice === "Scissors") || 
+    (user === "Paper" && computerChoice === "Rock") ||
+    (user === "Scissors" && computerChoice === "Paper")
+  ){
+    rpsi.innerHTML += "<br><br>You win!"
+  } else {
+    rpsi.innerHTML += "<br><br>You lose!"
+  }
+}
+
